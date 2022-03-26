f892b9322bdc1c6956cd79f2cd8dbd55ddf93986
User <User@STD08>
2022-03-26 18:58:10 +0500

    final

--- MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
+++ MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv)
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(1024, 768);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Tutorial 02");
+	glutCreateWindow("Tutorial 03");
 	glutDisplayFunc(RenderSceneOB);
 
 	GLenum res = glewInit();
@@ -29,9 +29,9 @@ int main(int argc, char** argv)
 	}
 	//glm::fvec3
 	glm::vec3 Vertices[3];
-	Vertices[0] = glm::vec3(-1.0f, -1.0f, 0.0f);
-	Vertices[2] = glm::vec3(1.0f, -1.0f, 0.0f);
-	Vertices[1] = glm::vec3(0.0f, 1.0f, 0.0f);
+	Vertices[0] = glm::vec3(1.0f, 1.0f, 0.0f);
+	Vertices[2] = glm::vec3(-1.0f, 1.0f, 0.0f);
+	Vertices[1] = glm::vec3(0.0f, -1.0f, 0.0f);
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
