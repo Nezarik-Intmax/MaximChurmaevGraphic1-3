7cbd9e3f58bd577c4e8e10d68048fe058d40edf9
User <User@STD08>
2022-03-26 18:54:39 +0500

    Добавьте файлы проекта.

---
+++ MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include "GL/glew.h";
+#include "GL/glut.h";
+#include "glm/glm.hpp";
+//#include "GL/freeglut.h";
+
+GLuint VBO;
+void RenderSceneOB() {
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDisableVertexAttribArray(0);
+	glutSwapBuffers();
+}
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Tutorial 02");
+	glutDisplayFunc(RenderSceneOB);
+
+	GLenum res = glewInit();
+	if (res != GLEW_OK) {
+		fprintf(stderr, "Error: '%s' \n", glewGetErrorString(res));
+		return 1;
+	}
+	//glm::fvec3
+	glm::vec3 Vertices[3];
+	Vertices[0] = glm::vec3(-1.0f, -1.0f, 0.0f);
+	Vertices[2] = glm::vec3(1.0f, -1.0f, 0.0f);
+	Vertices[1] = glm::vec3(0.0f, 1.0f, 0.0f);
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+	glutMainLoop();
+}
