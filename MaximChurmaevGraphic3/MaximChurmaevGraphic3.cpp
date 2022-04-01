95e74fa34aabdea053dc370dc56943fcfb1e81ab
User <User@STD08>
2022-04-01 17:55:38 +0500

    What?

--- MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
+++ MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
@@ -27,6 +27,7 @@ void RenderSceneCB() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
 	glDrawArrays(GL_TRIANGLES, 0, 3);
