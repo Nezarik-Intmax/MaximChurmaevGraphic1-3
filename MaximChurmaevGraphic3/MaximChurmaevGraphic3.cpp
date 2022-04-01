06b27628bc9ab359942988be77ce6444dc348274
User <User@STD08>
2022-04-01 17:29:28 +0500

    fin

--- MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
+++ MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
@@ -98,15 +98,12 @@ void RenderSceneCB() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	static float Scale = 0.0f;
 	Scale += 0.001f;
-	struct Matrix4f {
-		float m[4][4];
-	};
-	Matrix4f World;
-	World.m[0][0] = 1.0f; World.m[0][1] = 0.0f; World.m[0][2] = 0.0f; World.m[0][3] = sinf(Scale);
-	World.m[1][0] = 0.0f; World.m[1][1] = 1.0f; World.m[1][2] = 0.0f; World.m[1][3] = 0.0f;
-	World.m[2][0] = 0.0f; World.m[2][1] = 0.0f; World.m[2][2] = 1.0f; World.m[2][3] = 0.0f;
-	World.m[3][0] = 0.0f; World.m[3][1] = 0.0f; World.m[3][2] = 0.0f; World.m[3][3] = 1.0f;
-	glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World.m[0][0]);
+	glm::mat4x4 World;
+	World[0][0] = 1.0f; World[0][1] = 0.0f; World[0][2] = 0.0f; World[0][3] = sinf(Scale);
+	World[1][0] = 0.0f; World[1][1] = 1.0f; World[1][2] = 0.0f; World[1][3] = 0.0f;
+	World[2][0] = 0.0f; World[2][1] = 0.0f; World[2][2] = 1.0f; World[2][3] = 0.0f;
+	World[3][0] = 0.0f; World[3][1] = 0.0f; World[3][2] = 0.0f; World[3][3] = 1.0f;
+	glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World[0][0]);
 
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
