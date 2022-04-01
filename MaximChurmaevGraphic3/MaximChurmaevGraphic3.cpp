06a61a81541f8e0e0eb1ad94176ded06f5bf1729
User <User@STD08>
2022-04-01 18:14:38 +0500

    fin

--- MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
+++ MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
@@ -13,21 +13,20 @@ void RenderSceneCB() {
 	Vertices[2] = glm::vec3(-1.0f, 1.0f, 0.0f);
 	Vertices[1] = glm::vec3(0.0f, -1.0f, 0.0f);
 
-	Scale += 0.101f;
+	Scale += 0.001f;
 	glm::mat4x4 World;
 	World[0][0] = 1.0f; World[0][1] = 0.0f; World[0][2] = 0.0f; World[0][3] = sinf(Scale);
 	World[1][0] = 0.0f; World[1][1] = 1.0f; World[1][2] = 0.0f; World[1][3] = 0.0f;
 	World[2][0] = 0.0f; World[2][1] = 0.0f; World[2][2] = 1.0f; World[2][3] = 0.0f;
 	World[3][0] = 0.0f; World[3][1] = 0.0f; World[3][2] = 0.0f; World[3][3] = 1.0f;
-	Vertices[0] = World * glm::vec4(Vertices[0], 1.0f);
-	Vertices[1] = World * glm::vec4(Vertices[1], 1.0f);
-	Vertices[2] = World * glm::vec4(Vertices[2], 1.0f);
+	Vertices[0] = glm::vec4(Vertices[0], 1.0f) * World;
+	Vertices[1] = glm::vec4(Vertices[1], 1.0f) * World;
+	Vertices[2] = glm::vec4(Vertices[2], 1.0f) * World;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
 	glDrawArrays(GL_TRIANGLES, 0, 3);
