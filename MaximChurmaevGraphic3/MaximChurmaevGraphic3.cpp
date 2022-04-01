9a5692f21b08b89e908faaf9daac3e1b83081fae
User <User@STD08>
2022-04-01 19:34:13 +0500

    Zzzzz

--- MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
+++ MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
@@ -10,11 +10,12 @@ float Scale = 0.0f;
 #define ToRadian(x) ((x) * M_PI / 180.0f)
 #define ToDegree(x) ((x) * 180.0f / M_PI)
 void InitPers(glm::mat4x4& m) {
-	const float ar = 1;// m_persProj.Width / m_persProj.Height;
-	const float zNear = 0;// m_persProj.zNear;
-	const float zFar = 10000;// m_persProj.zFar;
+	const float ar = 1024 / 768;// m_persProj.Width / m_persProj.Height;
+	const float zNear = 1.0f;// m_persProj.zNear;
+	const float zFar = 1000.0f;// m_persProj.zFar;
 	const float zRange = zNear - zFar;
-	const float tanHalfFOV = tanf(ToRadian(70 / 2.0));
+	//const float tanHalfFOV = tanf(ToRadian(30.0f / 2.0));
+	const float tanHalfFOV = tanf(30.0f / 2.0);
 
 	m[0][0] = 1.0f / (tanHalfFOV * 1);
 	m[0][1] = 0.0f;
@@ -72,26 +73,34 @@ void RenderSceneCB() {
 	Vertices[1] = glm::vec3(0.0f, -1.0f, 0.0f);
 
 	Scale += 0.001f;
-	glm::mat4x4 World;
-	World[0][0] = 1.0f;		   World[0][1] = 0.0f;        World[0][2] = 0.0f;        World[0][3] = 0.0f;
-	World[1][0] = 0.0f;        World[1][1] = 1.0f;		  World[1][2] = 0.0f;        World[1][3] = 0.0f;
-	World[2][0] = 0.0f;        World[2][1] = 0.0f;        World[2][2] = 1.0f;		 World[2][3] = 0.0f;
-	World[3][0] = 0.0f;        World[3][1] = 0.0f;        World[3][2] = 0.0f;        World[3][3] = 1.0f;
-	glm::mat4x4 World;
-	World[0][0] = 1.0f; World[0][1] = 0.0f; World[0][2] = 0.0f; World[0][3] = sinf(Scale);
-	World[1][0] = 0.0f; World[1][1] = 1.0f; World[1][2] = 0.0f; World[1][3] = 0.0f;
-	World[2][0] = 0.0f; World[2][1] = 0.0f; World[2][2] = 1.0f; World[2][3] = 0.0f;
-	World[3][0] = 0.0f; World[3][1] = 0.0f; World[3][2] = 0.0f; World[3][3] = 1.0f;
-	glm::mat4x4 World;
-	World[0][0] = cosf(Scale); World[0][1] = -sinf(Scale); World[0][2] = 0.0f; World[0][3] = 0.0f;
-	World[1][0] = sinf(Scale); World[1][1] = cosf(Scale);  World[1][2] = 0.0f; World[1][3] = 0.0f;
-	World[2][0] = 0.0f;        World[2][1] = 0.0f;         World[2][2] = 1.0f; World[2][3] = 0.0f;
-	World[3][0] = 0.0f;        World[3][1] = 0.0f;         World[3][2] = 0.0f; World[3][3] = 1.0f;
-	InitPers(World);
-	//m_transformation = PersProjTrans * TranslationTrans * RotateTrans * ScaleTrans;
-	Vertices[0] = World * glm::vec4(Vertices[0], 1.0f);
-	Vertices[1] = World * glm::vec4(Vertices[1], 1.0f);
-	Vertices[2] = World * glm::vec4(Vertices[2], 1.0f);
+	glm::mat4x4 WorldPers;
+	WorldPers[0][0] = 1.0f;		   WorldPers[0][1] = 0.0f;        WorldPers[0][2] = 0.0f;        WorldPers[0][3] = 0.0f;
+	WorldPers[1][0] = 0.0f;        WorldPers[1][1] = 1.0f;		  WorldPers[1][2] = 0.0f;        WorldPers[1][3] = 0.0f;
+	WorldPers[2][0] = 0.0f;        WorldPers[2][1] = 0.0f;        WorldPers[2][2] = 1.0f;		 WorldPers[2][3] = 0.0f;
+	WorldPers[3][0] = 0.0f;        WorldPers[3][1] = 0.0f;        WorldPers[3][2] = 0.0f;        WorldPers[3][3] = 1.0f;
+	/*glm::mat4x4 WorldPos;
+	WorldPos[0][0] = 1.0f; WorldPos[0][1] = 0.0f; WorldPos[0][2] = 0.0f; WorldPos[0][3] = sinf(Scale);
+	WorldPos[1][0] = 0.0f; WorldPos[1][1] = 1.0f; WorldPos[1][2] = 0.0f; WorldPos[1][3] = 0.0f;
+	WorldPos[2][0] = 0.0f; WorldPos[2][1] = 0.0f; WorldPos[2][2] = 1.0f; WorldPos[2][3] = 0.0f;
+	WorldPos[3][0] = 0.0f; WorldPos[3][1] = 0.0f; WorldPos[3][2] = 0.0f; WorldPos[3][3] = 1.0f;
+	glm::mat4x4 WorldRot;
+	WorldRot[0][0] = cosf(Scale); WorldRot[0][1] = -sinf(Scale); WorldRot[0][2] = 0.0f; WorldRot[0][3] = 0.0f;
+	WorldRot[1][0] = sinf(Scale); WorldRot[1][1] = cosf(Scale);  WorldRot[1][2] = 0.0f; WorldRot[1][3] = 0.0f;
+	WorldRot[2][0] = 0.0f;        WorldRot[2][1] = 0.0f;         WorldRot[2][2] = 1.0f; WorldRot[2][3] = 0.0f;
+	WorldRot[3][0] = 0.0f;        WorldRot[3][1] = 0.0f;         WorldRot[3][2] = 0.0f; WorldRot[3][3] = 1.0f;
+	glm::mat4x4 WorldScl;
+	WorldScl[0][0] = sinf(Scale); WorldScl[0][1] = 0.0f;        WorldScl[0][2] = 0.0f;        WorldScl[0][3] = 0.0f;
+	WorldScl[1][0] = 0.0f;        WorldScl[1][1] = cosf(Scale); WorldScl[1][2] = 0.0f;        WorldScl[1][3] = 0.0f;
+	WorldScl[2][0] = 0.0f;        WorldScl[2][1] = 0.0f;        WorldScl[2][2] = sinf(Scale); WorldScl[2][3] = 0.0f;
+	WorldScl[3][0] = 0.0f;        WorldScl[3][1] = 0.0f;        WorldScl[3][2] = 0.0f;        WorldScl[3][3] = 1.0f;*/
+	InitPers(WorldPers);
+	//glm::mat4x4 m_transformation = WorldPers * WorldPos * WorldRot * WorldScl;
+	/*
+	
+	*/
+	Vertices[0] = WorldPers * glm::vec4(Vertices[0], 1.0f);
+	Vertices[1] = WorldPers * glm::vec4(Vertices[1], 1.0f);
+	Vertices[2] = WorldPers * glm::vec4(Vertices[2], 1.0f);
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
