c2a384cfdbc8cdd086365e142d582c1849b83924
Nezarik-Intmax <nezarikintmax@gmail.com>
2022-04-21 23:34:25 +0500

    g

--- MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
+++ MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
@@ -85,8 +85,8 @@ void RenderSceneCB(){
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
 	unsigned int Indices[] = {0, 3, 1,
 							   1, 3, 2,
 							   2, 3, 0,
