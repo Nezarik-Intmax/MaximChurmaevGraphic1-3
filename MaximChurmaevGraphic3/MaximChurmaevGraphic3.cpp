35d42ccdf9308f4fe166a2dedbf45fd89e9eeb98
Nezarik-Intmax <nezarikintmax@gmail.com>
2022-04-21 23:47:05 +0500

    f

--- MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
+++ MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
@@ -6,6 +6,36 @@
 
 GLuint VBO;
 GLuint IBO;
+
+GLuint gWorldLocation;
+
+
+static const char* pVS = "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+layout (location = 0) in vec3 Position;                                             \n\
+                                                                                    \n\
+uniform mat4 gWorld;                                                                \n\
+                                                                                    \n\
+out vec4 Color;                                                                     \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    gl_Position = gWorld * vec4(Position, 1.0);                                     \n\
+    Color = vec4(1.0, 1.0, 1.0, 1.0);                                   \n\
+}";
+
+static const char* pFS = "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+in vec4 Color;                                                                      \n\
+                                                                                    \n\
+out vec4 FragColor;                                                                 \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    FragColor = Color;                                                              \n\
+}";
 float Scale = 0.0f;
 #define M_PI 3.14159265358979323846
 #define ToRadian(x) ((x) * M_PI / 180.0f)
@@ -72,20 +102,24 @@ void RenderSceneCB(){
 	WorldScl[3][0] = 0.0f;        WorldScl[3][1] = 0.0f;        WorldScl[3][2] = 0.0f;        WorldScl[3][3] = 1.0f;
 	glm::fmat4 WorldPers;
 	InitPers(WorldPers, 0.1f, 100.0f, 1024, 768, 30);
-	glm::fmat4 m_transformation = glm::transpose(WorldPers) * WorldPos * WorldRot * glm::transpose(WorldScl);
-	/*
+	glm::fmat4* m_transformation = new glm::fmat4(glm::transpose(glm::transpose(WorldPers) * WorldPos * WorldRot * WorldScl));
 
-	*/
+	/*
 	m_transformation = glm::transpose(m_transformation);
 	Vertices[0] = m_transformation * glm::vec4(Vertices[0], 1.0f);
 	Vertices[1] = m_transformation * glm::vec4(Vertices[1], 1.0f);
 	Vertices[2] = m_transformation * glm::vec4(Vertices[2], 1.0f);
 	Vertices[3] = m_transformation * glm::vec4(Vertices[3], 1.0f);
+	*/
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+
+
+	glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, (const GLfloat*)m_transformation);
 
 	unsigned int Indices[] = {0, 3, 1,
 							   1, 3, 2,
@@ -104,6 +138,68 @@ void RenderSceneCB(){
 
 	glutSwapBuffers();
 }
+
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType){
+	GLuint ShaderObj = glCreateShader(ShaderType);
+
+	if(ShaderObj == 0){
+		fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+		exit(0);
+	}
+
+	const GLchar* p[1];
+	p[0] = pShaderText;
+	GLint Lengths[1];
+	Lengths[0] = strlen(pShaderText);
+	glShaderSource(ShaderObj, 1, p, Lengths);
+	glCompileShader(ShaderObj);
+	GLint success;
+	glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+	if(!success){
+		GLchar InfoLog[1024];
+		glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+		fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+		exit(1);
+	}
+
+	glAttachShader(ShaderProgram, ShaderObj);
+}
+
+static void CompileShaders(){
+	GLuint ShaderProgram = glCreateProgram();
+
+	if(ShaderProgram == 0){
+		fprintf(stderr, "Error creating shader program\n");
+		exit(1);
+	}
+
+	AddShader(ShaderProgram, pVS, GL_VERTEX_SHADER);
+	AddShader(ShaderProgram, pFS, GL_FRAGMENT_SHADER);
+
+	GLint Success = 0;
+	GLchar ErrorLog[1024] = {0};
+
+	glLinkProgram(ShaderProgram);
+	glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+	if(Success == 0){
+		glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+		fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+		exit(1);
+	}
+
+	glValidateProgram(ShaderProgram);
+	glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+	if(!Success){
+		glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+		fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+		exit(1);
+	}
+
+	glUseProgram(ShaderProgram);
+
+	gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
+	assert(gWorldLocation != 0xFFFFFFFF);
+}
 int main(int argc, char** argv){
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
@@ -119,5 +215,6 @@ int main(int argc, char** argv){
 		return 1;
 	}
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	CompileShaders();
 	glutMainLoop();
 }
