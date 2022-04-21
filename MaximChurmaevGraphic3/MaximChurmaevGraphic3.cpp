b381aa1f9f5434cf8b249fe1e0ed8048c2b27242
Nezarik-Intmax <nezarikintmax@gmail.com>
2022-04-21 22:45:28 +0500

    f

--- MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
+++ MaximChurmaevGraphic3/MaximChurmaevGraphic3.cpp
@@ -10,15 +10,15 @@ float Scale = 0.0f;
 #define M_PI 3.14159265358979323846
 #define ToRadian(x) ((x) * M_PI / 180.0f)
 #define ToDegree(x) ((x) * 180.0f / M_PI)
-void InitPers(glm::mat4x4& m, float zNear, float zFar, float width, float height, float fov) {
+void InitPers(glm::fmat4& m, float zNear, float zFar, float width, float height, float fov) {
 	const float ar = width / height;//1024 / 768;// m_persProj.Width / m_persProj.Height;
 	//const float zNear = 1.0f;// m_persProj.zNear;
 	//const float zFar = 1000.0f;// m_persProj.zFar;
 	const float zRange = zNear - zFar;
 	//const float tanHalfFOV = tanf(ToRadian(30.0f / 2.0));
-	const float tanHalfFOV = tanf(fov / 2.0);
+	const float tanHalfFOV = tanf(ToRadian(fov / 2.0));
 
-	m[0][0] = 1.0f / (tanHalfFOV * 1);
+	m[0][0] = 1.0f / (tanHalfFOV * ar);
 	m[0][1] = 0.0f;
 	m[0][2] = 0.0f;
 	m[0][3] = 0.0f;
@@ -39,60 +39,40 @@ void InitPers(glm::mat4x4& m, float zNear, float zFar, float width, float height
 	m[3][3] = 0.0f;
 
 }
-/*void InitPerspectiveProj(Matrix4f& m) {
-	const float ar = m_persProj.Width / m_persProj.Height;
-	const float zNear = m_persProj.zNear;
-	const float zFar = m_persProj.zFar;
-	const float zRange = zNear - zFar;
-	const float tanHalfFOV = tanf(ToRadian(m_persProj.FOV / 2.0));
-
-	m.m[0][0] = 1.0f / (tanHalfFOV * ar);
-	m.m[0][1] = 0.0f;
-	m.m[0][2] = 0.0f;
-	m.m[0][3] = 0.0f;
-
-	m.m[1][0] = 0.0f;
-	m.m[1][1] = 1.0f / tanHalfFOV;
-	m.m[1][2] = 0.0f;
-	m.m[1][3] = 0.0f;
-
-	m.m[2][0] = 0.0f;
-	m.m[2][1] = 0.0f;
-	m.m[2][2] = (-zNear - zFar) / zRange;
-	m.m[2][3] = 2.0f * zFar * zNear / zRange;
-
-	m.m[3][0] = 0.0f;
-	m.m[3][1] = 0.0f;
-	m.m[3][2] = 1.0f;
-	m.m[3][3] = 0.0f;
-}*/
 void RenderSceneCB() {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glm::vec3 Vertices[4];
-	Vertices[0] = glm::vec3(-1.0f, -1.0f, 0.5773f);
-	Vertices[1] = glm::vec3(0.0f, -1.0f, -1.15475);
-	Vertices[2] = glm::vec3(1.0f, -1.0f, 0.5773f);
-	Vertices[3] = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::fvec3 Vertices[4];
+	Vertices[0] = glm::fvec3(-1.0f, -1.0f, 0.5773f);
+	Vertices[1] = glm::fvec3(0.0f, -1.0f, -1.15475);
+	Vertices[2] = glm::fvec3(1.0f, -1.0f, 0.5773f);
+	Vertices[3] = glm::fvec3(0.0f, 1.0f, 0.0f);
 
 	Scale += 0.001f;
-	glm::mat4x4 WorldPos;
-	WorldPos[0][0] = 1.0f; WorldPos[0][1] = 0.0f; WorldPos[0][2] = 0.0f; WorldPos[0][3] = sinf(ToRadian(Scale));
+	glm::fmat4 WorldPos;
+	WorldPos[0][0] = 1.0f; WorldPos[0][1] = 0.0f; WorldPos[0][2] = 0.0f; WorldPos[0][3] = 0.0f;//sinf(ToRadian(Scale));
 	WorldPos[1][0] = 0.0f; WorldPos[1][1] = 1.0f; WorldPos[1][2] = 0.0f; WorldPos[1][3] = 0.0f;
 	WorldPos[2][0] = 0.0f; WorldPos[2][1] = 0.0f; WorldPos[2][2] = 1.0f; WorldPos[2][3] = 0.0f;
 	WorldPos[3][0] = 0.0f; WorldPos[3][1] = 0.0f; WorldPos[3][2] = 0.0f; WorldPos[3][3] = 1.0f;
-	glm::mat4x4 WorldRot;
-	WorldRot[0][0] = cosf(ToRadian(Scale)); WorldRot[0][1] = -sinf(ToRadian(Scale)); WorldRot[0][2] = 0.0f; WorldRot[0][3] = 0.0f;
+	glm::fmat4 WorldRot;
+	/*WorldRot[0][0] = cosf(ToRadian(Scale)); WorldRot[0][1] = -sinf(ToRadian(Scale)); WorldRot[0][2] = 0.0f; WorldRot[0][3] = 0.0f;
 	WorldRot[1][0] = sinf(ToRadian(Scale)); WorldRot[1][1] = cosf(ToRadian(Scale));  WorldRot[1][2] = 0.0f; WorldRot[1][3] = 0.0f;
 	WorldRot[2][0] = 0.0f;        WorldRot[2][1] = 0.0f;         WorldRot[2][2] = 1.0f; WorldRot[2][3] = 0.0f;
+	WorldRot[3][0] = 0.0f;        WorldRot[3][1] = 0.0f;         WorldRot[3][2] = 0.0f; WorldRot[3][3] = 1.0f;*/
+
+
+	WorldRot[0][0] = cosf(ToRadian(Scale)); WorldRot[0][1] = 0.0f; WorldRot[0][2] = -sinf(ToRadian(Scale)); WorldRot[0][3] = 0.0f;
+	WorldRot[1][0] = 0.0f; WorldRot[1][1] = 1.0f;  WorldRot[1][2] = 0.0f; WorldRot[1][3] = 0.0f;
+	WorldRot[2][0] = sinf(ToRadian(Scale));        WorldRot[2][1] = 0.0f;         WorldRot[2][2] = cosf(ToRadian(Scale)); WorldRot[2][3] = 0.0f;
 	WorldRot[3][0] = 0.0f;        WorldRot[3][1] = 0.0f;         WorldRot[3][2] = 0.0f; WorldRot[3][3] = 1.0f;
-	glm::mat4x4 WorldScl;
-	WorldScl[0][0] = sinf(ToRadian(Scale)); WorldScl[0][1] = 0.0f;        WorldScl[0][2] = 0.0f;        WorldScl[0][3] = 0.0f;
-	WorldScl[1][0] = 0.0f;        WorldScl[1][1] = cosf(ToRadian(Scale)); WorldScl[1][2] = 0.0f;        WorldScl[1][3] = 0.0f;
-	WorldScl[2][0] = 0.0f;        WorldScl[2][1] = 0.0f;        WorldScl[2][2] = sinf(ToRadian(Scale)); WorldScl[2][3] = 0.0f;
+
+	glm::fmat4 WorldScl;
+	WorldScl[0][0] = 0.1f; WorldScl[0][1] = 0.0f;        WorldScl[0][2] = 0.0f;        WorldScl[0][3] = 0.0f;
+	WorldScl[1][0] = 0.0f;        WorldScl[1][1] = 0.1f; WorldScl[1][2] = 0.0f;        WorldScl[1][3] = 0.0f;
+	WorldScl[2][0] = 0.0f;        WorldScl[2][1] = 0.0f;        WorldScl[2][2] = 0.1f; WorldScl[2][3] = 0.0f;
 	WorldScl[3][0] = 0.0f;        WorldScl[3][1] = 0.0f;        WorldScl[3][2] = 0.0f;        WorldScl[3][3] = 1.0f;
-	glm::mat4x4 WorldPers;
-	InitPers(WorldPers, 1.0f, 1000.0f, 1024, 768, 30);
-	glm::mat4x4 m_transformation = glm::transpose(WorldPers) * WorldPos * WorldRot * WorldScl;
+	glm::fmat4 WorldPers;
+	InitPers(WorldPers, 0.1f, 100.0f, 1024, 768, 30);
+	glm::fmat4 m_transformation = glm::transpose(WorldPers) * WorldPos * WorldRot * glm::transpose(WorldScl);
 	/*
 	
 	*/
@@ -104,6 +84,9 @@ void RenderSceneCB() {
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	unsigned int Indices[] = {0, 3, 1,
 							   1, 3, 2,
 							   2, 3, 0,
@@ -113,13 +96,9 @@ void RenderSceneCB() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
 
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
 
 	glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
-	//glDrawArrays(GL_TRIANGLES, 0, 4);
+	//glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	glDisableVertexAttribArray(0);
 
